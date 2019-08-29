105752457638fbc01cfbe3296b25db63af7a831b
82106 <82106@LAPTOP-0FBIVP0T>
2019-08-29 19:20:19 +0900

    adsgadgsdsga

--- src/jung/jae/com/Arraysort12.java
+++ src/jung/jae/com/Arraysort12.java
@@ -7,7 +7,7 @@ public class Arraysort12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int [] array = {48,7,1,4,2};
-		int length = array.length;
+		int length = array.length; 
 		for(int j=0 ; j<length ; j++) {
 			for(int i=0 ; i<length-1 ; i++) {
 				if(array[i] > array[i+1]) {
