b71a99ce1a65c8c967802ad5bd88171ef6d30684
82106 <82106@LAPTOP-0FBIVP0T>
2019-08-30 21:51:49 +0900

    권수가 복병이다

--- src/jung/jae/com/mathod/Board.java
+++ src/jung/jae/com/mathod/Board.java
@@ -1,10 +1,34 @@
 package jung.jae.com.mathod;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Board {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+	int id;
+	String title;
+	String content;
+	User writer;
+	Date wdate;
+	Date udate;
+
+	public boolean insert(Board board) {
+		System.out.println("insert");
+		return true;
 
 	}
-
+	public boolean edit(Board board) {
+		System.out.println("edit");
+		return true;
+	}
+	public Board getBoard(int id) {
+		Board result = new Board();
+		return result;
+	}
+	public List<Board>getList(){
+		List<Board>result = new ArrayList<Board>();
+		return result;
+	}
 }
--- src/jung/jae/com/mathod/MainClass.java
+++ src/jung/jae/com/mathod/MainClass.java
@@ -1,24 +1,29 @@
 package jung.jae.com.mathod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SungJuk rec = new SungJuk();
-		
-		rec.stdNum = JOptionPane.showInputDialog("학번");
-		rec.stdName = JOptionPane.showInputDialog("이름");
-		rec.kor = Integer.parseInt(JOptionPane.showInputDialog("국어"));
-		rec.eng = Integer.parseInt(JOptionPane.showInputDialog("영어"));
-		rec.mat = Integer.parseInt(JOptionPane.showInputDialog("수학"));
-		
-		System.out.println(rec.stdNum + rec.stdName + "의 성적입니다.");
-		
-		rec.sum();
-		rec.ave();
+
+		List<Integer> list = new ArrayList<Integer>();
+		while(list.size() <=50) {
+			//System.out.println(list.size());
+			double randomNum = Math.random();
+			int random = (int)(randomNum * 100)+1;
+			if(list.contains(random)) {
+				continue;
+			}else {
+				list.add(random);
+			}
+		}
+		for(int i = 0 ; i < list.size() ; i++) {
+			System.out.println("randomNum : " + list.get(i));
+		}
 
 	}
-
 }
+
