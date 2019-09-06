7acb9b36737916b28d86f6ebb60302d54da8babb
82106 <82106@LAPTOP-0FBIVP0T>
2019-09-07 08:56:18 +0900

    dfgsaasggdsa

---
+++ src/jung/jae/com/mathod/Numtest1234.java
@@ -0,0 +1,22 @@
+package jung.jae.com.mathod;
+
+public class Numtest1234 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean result = false;
+		int Quo;
+		int Rem;
+		int Decimal = 26;
+		int birary;
+		while(result == false) {
+			if(){
+
+			}else if(Quo == 0){ 
+				result = true;
+			}
+
+		}
+	}
+
+}
