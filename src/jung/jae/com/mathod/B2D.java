1cf6f6f862d4f750a3e181f975e94c296809b37a
82106 <82106@LAPTOP-0FBIVP0T>
2019-09-20 21:50:30 +0900

    gdsgads

---
+++ src/jung/jae/com/mathod/B2D.java
@@ -0,0 +1,16 @@
+package jung.jae.com.mathod;
+
+import javax.swing.JOptionPane;
+
+public class B2D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String A = JOptionPane.showInputDialog("2진수");
+		String[] B = A.split("");
+		for( int i = 0 ; i<A.length() ; i++) {
+			
+		}
+	}
+
+}
---
+++ src/jung/jae/com/mathod/GWONSUBS.java
@@ -0,0 +1,11 @@
+package jung.jae.com.mathod;
+
+public class GWONSUBS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub	
+		double i = Math.pow(2,0);
+		System.out.println(i);
+	}
+
+}
